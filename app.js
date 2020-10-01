938a3f0594bddaff1d0159727cdd1b5c2214c5fa
Miranda Riley <mirandariley@Mirandas-Air-2.attlocal.net>
2020-09-30 23:37:45 -0500

    added color change

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let quiz = document.querySelector('.seinfeld-quiz');
+let question1 = document.querySelector('#q1Title')
 
 quiz.addEventListener('click', e => {
     console.log('clicked');
     console.log(e.target.name);
     console.log(e.target.value);
+    if(e.target.name == 'q1'){
+        if (e.target.value == 'A'){
+            question1.classList.add('correct');
+            question1.classList.remove('wrong');
+
+            console.log('correct!');
+        } else {
+            console.log('false');
+            question1.classList.add('wrong');
+            question1.classList.remove('correct');
+
+
+        }
+        console.log('you clicked one!')
+    }
 
     // console.log(e.target.name.value);
 
