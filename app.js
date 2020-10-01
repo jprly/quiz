64b0562424f10f9817d72db214c251c468f2f9ff
Miranda Riley <mirandariley@Mirandas-Air-2.attlocal.net>
2020-10-01 05:53:04 -0500

    refactored form check

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 let quiz = document.querySelector('.seinfeld-quiz');
-let question1 = document.querySelector('#q1Title')
+let question1 = document.querySelector('#q1Title');
+let question2 = document.querySelector('#q2Title');
+let question3 = document.querySelector('#q3Title');
+let question4 = document.querySelector('#q4Title');
+
+let gradeQuestion = (question, answer, e) => {
+    if (e.target.value == answer){
+        question.classList.add('correct'); 
+        question.classList.remove('wrong');
+    } else {
+        question.classList.add('wrong');
+        question.classList.remove('correct');
+    }
+}
 
 quiz.addEventListener('click', e => {
-    console.log('clicked');
-    console.log(e.target.name);
-    console.log(e.target.value);
-    if(e.target.name == 'q1'){
-        if (e.target.value == 'A'){
-            question1.classList.add('correct');
-            question1.classList.remove('wrong');
-
-            console.log('correct!');
-        } else {
-            console.log('false');
-            question1.classList.add('wrong');
-            question1.classList.remove('correct');
-
-
-        }
-        console.log('you clicked one!')
+    if (e.target.name == 'q1'){
+        gradeQuestion(question1, 'A', e);
+    }
+    if (e.target.name == 'q2'){
+        gradeQuestion(question2, 'B', e);
+    }
+    if (e.target.name == 'q3'){
+        gradeQuestion(question3, 'A', e);
+    }
+    if (e.target.name == 'q4'){
+        gradeQuestion(question4, 'A', e);
     }
 
-    // console.log(e.target.name.value);
+    
 
-})
+
+});
